c3b3ddc11a8e0eb5fadd05140ae5c18236a8a15d
github username <github email address>
2019-01-18 12:33:34 -0500

    solved fizzbuzz problem

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,21 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let counter = 1; counter <= n; counter++) {
+        if (counter % 3 === 0 && counter % 5 === 0) {
+            console.log('fizzbuzz')
+        }
+        else if(counter % 3 === 0){
+            console.log('fizz')
+        }
+        else if(counter % 5 === 0){
+            console.log('buzz')
+        }
+        else{
+            console.log(counter)
+        }        
+    }
+}
 
 module.exports = fizzBuzz;
