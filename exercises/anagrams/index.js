b74f7a1f8d748e506f2586d7e0ff8e0d62174b63
github username <github email address>
2019-02-02 09:38:54 -0500

    solved anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return stringCleaner(stringA) === stringCleaner(stringB)
+}
+
+function stringCleaner(string){
+    return string.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
+
+
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//     const charMapA = mapHelper(stringA)
+//     const charMapB = mapHelper(stringB)
+//     if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//         return false
+//     }
+//     for (let char in charMapA) {
+//         if (charMapA[char] !== charMapB[char]) {
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+// function mapHelper(string) {
+//     const charMap = {}
+//     for (let char of string.replace(/[^\w]/g).toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1
+//     }
+//     return charMap
+// }
